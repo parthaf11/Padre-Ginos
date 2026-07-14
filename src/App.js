dae0f859ac1e959c47c101a3a7cac6281a810110
Partha Sarathi <partha6252472@gmail.com>
2026-07-14 22:07:15 +0530

    jj

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App=()=>{
+  return React.createElement('div',{},React.createElement('h1',{},'Padre Ginos'),
+    
+  );
+};
+
+const container =document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
